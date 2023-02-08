7981b0f98d277b05a69e43066e8011a0a22b70e5
jaimin.halari <jaimin.halari@promptsoftech.in>
2023-02-08 12:10:26 +0530

    - Implement dataBinding

--- app/src/main/java/com/example/mvvm_databinding_navcomponent_coroutins_hilt_retrofit_room_flow/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_databinding_navcomponent_coroutins_hilt_retrofit_room_flow/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.mvvm_databinding_navcomponent_coroutins_hilt_retrofit_room_f
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.mvvm_databinding_navcomponent_coroutins_hilt_retrofit_room_flow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //set data Binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.test = "Hello Jaimin..!"
+
     }
 }
